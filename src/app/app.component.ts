a14309fd23c33df4a999e8e387b5c9d2c0a7fc14
Дарья <batterfly74@yandex.ru>
2023-03-10 01:45:42 +0500

    Добавьте файлы проекта.

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h1>Добро пожаловать {{name}}!</h1>
+                 <br>
+                 <q>Patience is a key element of success</q>
+                 <br>
+                 <cite>Bill Gates</cite>`
+})
+export class AppComponent {
+    name = '';
+}
